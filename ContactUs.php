659ca5be4e617a9f6adc1a44ea910374c6ebb4b2
Alex Poulsen <totallynota2ndacct@gmail.com>
2015-01-30 18:23:55 -0500

    Add everything
    All of the files are in here, updated

---
+++ ContactUs.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Contact Us</title>
+    <link rel="stylesheet" type="text/css"  href="style/structure.css" />
+    <link rel="stylesheet" type="text/css" href="style/navmenu.css" />
+    <link rel="shortcut icon" href="Favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" href="style/emailForm.css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+
+<body>
+	<!--Start navmenu-->
+    <div class="navtitle">
+    <img src="images/navtitle.jpg" width="180" height="45" alt="logo" />
+    </div>
+    <div class="nav">
+    <ul class="navmenu">
+    <!-- <span class="rightArrow">&#9654;</span>	<span class="downArrow">&#9660;</span> -->
+        <li><a href="index.htm">Home</a></li>
+        <li><a href="#">About Us</a><span class="downArrow">&#9660;</span>
+            <ul class="sub1">
+                <li><a href="TheTeam.htm">The Members</a></li>
+                <li><a href="TheMentors.htm">The Mentors</a></li>
+                <li><a href="AboutTeam1518.htm">Team 1518</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Our Bots</a><span class="downArrow">&#9660;</span>
+        	<ul class="sub1">
+            	<li><a href="CurrentBots.htm">This Year</a></li>
+                <li><a href="PreviousBots.htm">Previous Years</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Media</a><span class="downArrow">&#9660;</span>
+            <ul class="sub1">
+                <li><a href="Photos.htm">Pictures</a></li>
+                <li><a href="Videos.htm">Videos</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Events</a><span class="downArrow">&#9660;</span>
+        	<ul class="sub1">
+            	<li><a href="http://www.usfirst.org/roboticsprograms/frc">FRC</a><span class="rightArrow">&#9654;</span>
+                	<ul class="sub2">
+                    	<li><a href="http://ruckus.penfieldrobotics.com/">Ruckus</a></li>
+                    </ul>
+                </li>
+                <li><a href="http://www.usfirst.org/roboticsprograms/ftc">FTC</a></li>
+                <li><a href="http://www.usfirst.org/roboticsprograms/fll">FLL</a></li>
+            </ul>
+        </li>
+        <li><a href="Sponsors.htm">Sponsors</a></li>
+        <li><a href="#">Registration</a><span class="downArrow">&#9660;</span>
+        	<ul class="sub1">
+            <li><a href="https://my.usfirst.org/stims/site.lasso">Students</a></li>
+            <li><a href="https://my.usfirst.org/frc/tims/site.lasso">Mentors</a></li>
+            </ul>
+        </li>
+        <li><a href="AboutFirst.htm">About FIRST</a></li>
+        <li><a href="ContactUs.php">Contact Us</a></li>
+    </ul>
+    </div>
+    <!--End navmenu-->
+    
+    <br /><br /><br />
+    
+	<!--Start header-->
+    <div class="header">
+    <center><img class="banner" src="images/banner.jpg" width="1000" height="250" alt="FIRST Raider Robotics Tribal Skull Logo" /></center>
+	<h1>FIRST Raider Robotics Team 1518</h1>
+    <h2>Pal-Mac High School</h2>
+	</div>
+    <!--End header-->
+    
+    <hr/><br />
+    
+    
+<!-- **************************************************  MAIN CONTENT START  ************************************************** -->
+<!-- ************************************************************************************************************************** -->    
+    <!--Start container-->
+    <div class="container">
+    
+    <center>
+    <h2>Contact Us</h2>
+    </center>
+    <hr />
+    
+    <!--Start left-->
+    <div class="container" id="left">
+    
+    <a href="http://www.palmaccsd.org/" style="float:left"><img src="images/PMCSD-logo.jpg" height="80" width="80" alt="Pal-Mac High School Logo" style="margin-right:10px"/></a>
+    <h2>Pal-Mac School District</h2>
+    <p><a href="http://www.palmaccsd.org/">Pal-Mac School District Homepage</a><br />
+    <a href="http://www.palmaccsd.org/High.cfm">Pal-Mac High School</a></p>
+    <br />
+    
+    <p>In order to prove you are human you will need to enter a reCAPTCHA in order to view email addresses.</p>
+    <br />
+    
+    <h2>Kevin Schlegel</h2>
+    <p>Lead Mentor
+    <br />
+    <a href="http://www.google.com/recaptcha/mailhide/d?k=01Xe2u3V02MZexmoAc3umLAQ==&amp;c=J0vbqa_LHC2TQT4MAl_bM-ImLSbUHtpLbRFEHxh9p3k=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501Xe2u3V02MZexmoAc3umLAQ\75\75\46c\75J0vbqa_LHC2TQT4MAl_bM-ImLSbUHtpLbRFEHxh9p3k\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="Reveal this e-mail address">Click to show Email.</a>
+    </p>
+    <br />
+    
+    <h2>Web Master</h2>
+    <p>
+    <a href="http://www.google.com/recaptcha/mailhide/d?k=01Tl0KvY9ABSuvbsRUJbxydQ==&amp;c=NYvv3owgsexxJS5TLVd3iQOqtSsfSIvO4EB_SpvTQKw=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501Tl0KvY9ABSuvbsRUJbxydQ\75\75\46c\75NYvv3owgsexxJS5TLVd3iQOqtSsfSIvO4EB_SpvTQKw\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" title="Reveal this e-mail address">Click to show Email.</a>
+    </p>
+    <br />
+    
+    </div>
+    <!--End left-->
+    
+    
+    <!--Start right-->
+    <div class="container" id="right">
+  
+    <h2>Send Us an Email</h2>
+    
+    <p>Form is currently down due to spam. Will be back up shortly.</p>
+    
+    </div>
+    <!--End right-->
+    
+    </div>
+    <!--End container-->
+<!-- **************************************************  MAIN CONTENT END  **************************************************** -->
+<!-- ************************************************************************************************************************** --> 
+    
+    
+   <!--Start footer-->
+    <div class="footer">
+    <center><p>Pal-Mac Radier Robotics - Team 1518<br />
+	Copyright &copy; 2013 - All Rights Reserved<br />
+    Designed by Jackson DeMeyers - 2013<br />
+    Powered by <a href="http://www.sevaa.com/">Sevaa Group</a>
+	</p>
+    <p>
+    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+        <img style="border:0;width:88px;height:31px"
+            src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+            alt="Valid CSS!" />
+    </a>
+    <a href="http://validator.w3.org/check?uri=referer"><img
+      src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
+  	</p>
+	</center>
+    </div>
+    <!--End footer-->
+</body>
+</html>
